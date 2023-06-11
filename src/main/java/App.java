e5dcf2ac26fa1104fd8e770c2e90f28bd75a508d
Itm-info <itm@infotech.net.ua>
2023-06-11 13:40:29 +0300

    Initial Commit

---
+++ src/main/java/App.java
@@ -0,0 +1,17 @@
+import org.json.*;
+
+public class App {
+
+    public static void main(String[] args) {
+        JSONStringer jsonStringer = new JSONStringer();
+        jsonStringer.object();
+        jsonStringer.key("name").value("Name");
+        jsonStringer.key("surname").value("SurName");
+        jsonStringer.endObject();
+
+        String str = jsonStringer.toString();
+        JSONObject jsonObject = new JSONObject(str);
+
+        System.out.println("Final JSONObject: " + jsonObject);
+    }
+}
